60cffd48144e99b1ee19adb8dc6ad7bc04c220ad
Muhammad Fariz <mfariz8701@gmail.com>
2025-12-07 11:38:04 +0700

    hapus plugin babel

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,6 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
-      require.resolve("expo-router/babel"),
       [
         "module:react-native-dotenv",
         {
